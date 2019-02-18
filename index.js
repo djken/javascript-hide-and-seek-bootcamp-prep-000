feee092f0369c70f3a5563b057dfc4d3b1936d9e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-18 13:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("div.target")
+}
+
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list')
+  
+  for ( let i = 0, l = rankedList.length; i < l ; i++){
+    
+    var children = rankedList[i].children;
+    
+    for ( let j = 0, k = children.length; j <k; j++) {
+      
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n
+      
+    }
+  }
+}
+
+function deepestChild(){
+  var node = document.getElementById('grand-node')
+  var childNode = node.children[0]
+  
+  while (childNode){
+    node = childNode
+    childNode = node.children[0];
+  }
+  return node;
+}
